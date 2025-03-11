ea775ef71321090506541c5ba59351a04c31ba7a
den <leeden18@mail.ru>
2025-03-11 16:17:42 +0300

    feat: create main.py Set up the application launch in main.py and changed the url to get the UI documentation faster

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from fastapi.openapi.docs import (
+    get_swagger_ui_html,
+    get_swagger_ui_oauth2_redirect_html,
+)
+import uvicorn
+
+
+app = FastAPI()
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - Swagger UI",
+        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+        swagger_js_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js",
+        swagger_css_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css",
+    )
+
+
+@app.get("/")
+def func():
+    return "Hello, World!"
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
