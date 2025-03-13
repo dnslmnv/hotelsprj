5039c5fcd30fb8c89d782b1846aacfbb8764a8ee
den <leeden18@mail.ru>
2025-03-14 00:36:10 +0300

    feat: add pagination parameter with DI

--- hotels.py
+++ hotels.py
@@ -1,5 +1,7 @@
-from fastapi import Query, Body, APIRouter
+from fastapi import Query, Body, APIRouter, Depends
 from schemas.hotels import Hotel, HotelPATCH
+from dependencies import PaginationDep
+
 
 router = APIRouter(prefix="/hotels", tags=["Отели"])
 
@@ -145,10 +147,9 @@ hotels = [
 
 @router.get("")
 def get_hotels(
+        pagination: PaginationDep,
         id: int | None = Query(None, description="Айди отеля"),
-        title: str | None = Query(None, description="Название отеля"),
-        page: int | None = Query(None, description="Страница отображения", gt = 1),
-        per_page: int | None = Query(None, description="Количество элементов на странице", gt = 1, lt = 30)
+        title: str | None = Query(None, description="Название отеля")
 ):
     hotels_ = []
     for hotel in hotels:
@@ -158,9 +159,9 @@ def get_hotels(
             continue
         hotels_.append(hotel)
 
-    if page and per_page:
+    if pagination.page and pagination.per_page:
         #chain slicing
-        return hotels_[per_page * (page - 1):][:per_page]
+        return hotels_[pagination.per_page * (pagination.page - 1):][:pagination.per_page]
     return hotels_
 
 
