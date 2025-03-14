b398b25a06fdfb211056918e009483d3dc2dd1c6
den <leeden18@mail.ru>
2025-03-14 13:26:16 +0300

    feat: hotels model

--- src/database.py
+++ src/database.py
@@ -1,19 +1,16 @@
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
-from sqlalchemy import text
+from sqlalchemy.orm import DeclarativeBase
 from src.config import settings
-import asyncio
+
 
 engine = create_async_engine(settings.DB_URL)
 
 
 async_session_maker = async_sessionmaker(bind=engine, expire_on_commit=False)
 
+
 session = async_session_maker()
 
-#raw sql request
-# async def func():
-#     async with engine.begin() as conn:
-#         res = await conn.execute(text("SELECT version()"))
-#         print(res.fetchone())
-#
-# asyncio.run(func())
+
+class Base(DeclarativeBase):
+    pass
