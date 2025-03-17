3cfae3c52994a99374ba17724d4b0bbbf78d27c1
den <leeden18@mail.ru>
2025-03-17 10:06:00 +0300

    feat: add db log to console in post route

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -46,6 +46,7 @@ async def create_hotels(
 ):
     async with async_session_maker() as session:
         add_hotel_stmt = insert(HotelsOrm).values(**hotel_data.model_dump())
+        print(add_hotel_stmt.compile(compile_kwargs={"literal_binds": True}))
         await session.execute(add_hotel_stmt)
         await session.commit()
     return {"status": "OK"}
