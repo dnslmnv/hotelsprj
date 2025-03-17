d4fa5dceba432537eb16352ab88ec4f172c54a3a
den <leeden18@mail.ru>
2025-03-17 10:02:41 +0300

    feat: rewrite post handle to work with sqlalchemy and db

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -1,148 +1,12 @@
 from fastapi import Query, Body, APIRouter
+
+from sqlalchemy import insert
 from src.schemas.hotels import Hotel, HotelPATCH
 from src.api.dependencies import PaginationDep
-
+from src.database import async_session_maker
+from src.models.hotels import HotelsOrm
 
 router = APIRouter(prefix="/hotels", tags=["Отели"])
-
-
-# hotels = [
-#     {
-#         "id": 1,
-#         "title": "Sochi",
-#         "name": "5star"
-#     },
-#     {
-#         "id": 2,
-#         "title": "Dubai",
-#         "name": "Jingle"
-#     },
-#     {
-#         "id": 3,
-#         "title": "Paris",
-#         "name": "Luxury"
-#     },
-#     {
-#         "id": 4,
-#         "title": "Tokyo",
-#         "name": "Premium"
-#     },
-#     {
-#         "id": 5,
-#         "title": "London",
-#         "name": "Elite"
-#     },
-#     {
-#         "id": 6,
-#         "title": "Beijing",
-#         "name": "Diamond"
-#     },
-#     {
-#         "id": 7,
-#         "title": "Moscow",
-#         "name": "Platinum"
-#     },
-#     {
-#         "id": 8,
-#         "title": "Sydney",
-#         "name": "Gold"
-#     },
-#     {
-#         "id": 9,
-#         "title": "Bangkok",
-#         "name": "Silver"
-#     },
-#     {
-#         "id": 10,
-#         "title": "Singapore",
-#         "name": "Bronze"
-#     },
-#     {
-#         "id": 11,
-#         "title": "Rome",
-#         "name": "Crystal"
-#     },
-#     {
-#         "id": 12,
-#         "title": "Barcelona",
-#         "name": "Pearl"
-#     },
-#     {
-#         "id": 13,
-#         "title": "Berlin",
-#         "name": "Ruby"
-#     },
-#     {
-#         "id": 14,
-#         "title": "Madrid",
-#         "name": "Emerald"
-#     },
-#     {
-#         "id": 15,
-#         "title": "Prague",
-#         "name": "Sapphire"
-#     },
-#     {
-#         "id": 16,
-#         "title": "Vienna",
-#         "name": "Amethyst"
-#     },
-#     {
-#         "id": 17,
-#         "title": "Athens",
-#         "name": "Topaz"
-#     },
-#     {
-#         "id": 18,
-#         "title": "Stockholm",
-#         "name": "Opal"
-#     },
-#     {
-#         "id": 19,
-#         "title": "Copenhagen",
-#         "name": "Turquoise"
-#     },
-#     {
-#         "id": 20,
-#         "title": "Helsinki",
-#         "name": "Aquamarine"
-#     },
-#     {
-#         "id": 21,
-#         "title": "Oslo",
-#         "name": "Peridot"
-#     },
-#     {
-#         "id": 22,
-#         "title": "Zurich",
-#         "name": "Garnet"
-#     },
-#     {
-#         "id": 23,
-#         "title": "Geneva",
-#         "name": "Onyx"
-#     },
-#     {
-#         "id": 24,
-#         "title": "Milan",
-#         "name": "Jade"
-#     },
-#     {
-#         "id": 25,
-#         "title": "Venice",
-#         "name": "Corals"
-#     },
-#     {
-#         "id": 26,
-#         "title": "Amsterdam",
-#         "name": "Moonstone"
-#     },
-#     {
-#         "id": 27,
-#         "title": "Dublin",
-#         "name": "Sunstone"
-#     }
-# ]
 
 
 @router.get("")
@@ -175,17 +39,15 @@ def delete_hotels(
 
 
 @router.post("")
-def create_hotels(
-        hotel_data: Hotel = Body(openapi_examples={"1": {"summary": "Tver", "value": {"title": "Tver", "name": "Volga"}},
-                                                   "2": {"summary": "Moscow", "value": {"title": "Moscow", "name": "4 Seasons"}}
+async def create_hotels(
+        hotel_data: Hotel = Body(openapi_examples={"1": {"summary": "Tver", "value": {"title": "Volga", "location": "Tver"}},
+                                                   "2": {"summary": "Moscow", "value": {"title": "Tver", "location": "Moscow"}}
                                                    })
 ):
-    global hotels
-    hotels.append({
-        "id": hotels[-1]["id"] + 1,
-        "title": hotel_data.title,
-        "name" : hotel_data.name
-    })
+    async with async_session_maker() as session:
+        add_hotel_stmt = insert(HotelsOrm).values(**hotel_data.model_dump())
+        await session.execute(add_hotel_stmt)
+        await session.commit()
     return {"status": "OK"}
 
 
@@ -197,7 +59,7 @@ def edit_hotels_put(
     global hotels
     hotel = [hotel for hotel in hotels if hotel["id"] == hotel_id][0]
     hotel["title"] = hotel_data.title
-    hotel["name"] = hotel_data.name
+    hotel["location"] = hotel_data.location
     return {"status": "OK"}
 
 
@@ -212,6 +74,6 @@ def edit_hotels_patch(
     hotel = [hotel for hotel in hotels if hotel["id"] == hotel_id][0]
     if hotel_data.title:
         hotel["title"] = hotel_data.title
-    if hotel_data.name:
-        hotel["name"] = hotel_data.name
+    if hotel_data.location:
+        hotel["location"] = hotel_data.location
     return {"status": "OK"}
