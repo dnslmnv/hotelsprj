75c298ffc1bcc512c81ce73a1a7b3ef696d43d02
den <leeden18@mail.ru>
2025-03-25 13:19:03 +0300

    feat: new model - users

---
+++ src/migrations/versions/2025_03_25_1316-2088f04673e7_add_users.py
@@ -0,0 +1,35 @@
+"""add-users
+
+Revision ID: 2088f04673e7
+Revises: b93c0c9c6d1a
+Create Date: 2025-03-25 13:16:46.296321
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "2088f04673e7"
+down_revision: Union[str, None] = "b93c0c9c6d1a"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "users",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("email", sa.String(length=200), nullable=False),
+        sa.Column("password", sa.String(length=200), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
+
+
+def downgrade() -> None:
+    op.drop_table("users")
+
